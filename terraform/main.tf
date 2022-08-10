e009c63c713afa4ea85e7f6770e1352f5f6e495d
Taiqp <taiqp90@gmail.com>
2022-08-10 21:54:51 +0700

    debug

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,16 +4,16 @@ resource "azurerm_linux_virtual_machine" "lavoro" {
 
   for_each            = toset(var.Role)
   name                = each.value
+  size                = var.Size(each.value)
+  network_interface_ids = [azurerm_network_interface.lavoro[each.key].id]
+
   resource_group_name = azurerm_resource_group.lavoro.name
   location            = azurerm_resource_group.lavoro.location
-  size                = var.Size(each.value)
-
   admin_username      = "lavoro"
-  network_interface_ids = [azurerm_network_interface.lavoro[each.key].id]
 
   admin_ssh_key {
     username   = "lavoro"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("/home/tqphat/lavoro/terraform/id_rsa.pub")
   }
 
   os_disk {
