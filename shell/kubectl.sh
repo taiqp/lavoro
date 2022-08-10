6b997cf65d0f46b4c113d17f9f762fa11c5f54c9
Ubuntu <azureuser@insomma.onafkjtll01ulcris0wtvaq20e.bx.internal.cloudapp.net>
2022-08-10 16:02:56 +0000

    Many scripts

---
+++ shell/kubectl.sh
@@ -0,0 +1,5 @@
+# Kubectl
+cd /tmp/
+sudo curl -kLO https://storage.googleapis.com/kubernetes-release/release/v1.8.0/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
