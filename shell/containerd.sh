9514596a348b1efe5dc13851d9eb91cccfa49678
root <root@master.ndng1e5tytnunb2vmjn5f02u0a.bx.internal.cloudapp.net>
2022-10-10 08:50:51 +0000

    add containerd

---
+++ shell/containerd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install \
+ ca-certificates \
+ curl \
+ gnupg \
+ wget \
+ vim \
+ git \
+ net-tools \
+ lsb-release -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install containerd.io -y
+
+
